b1ce59b436d689c1b1dc92fee94cd9dade3a93c3
mahesh <dananjayamahesh@gmail.com>
2022-04-28 13:02:19 +0100

    add -y force yes

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
-sudo apt install libmemcached-dev libpapi-dev memcached
-sudo apt install gcc-7 g++-7
+sudo apt install -y libmemcached-dev libpapi-dev memcached
+sudo apt install -y gcc-7 g++-7
+sudo apt-get install -y build-essential g++ python-dev autotools-dev libicu-dev libbz2-dev
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 10
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 20
-sudo apt-get install build-essential g++ python-dev autotools-dev libicu-dev libbz2-dev 
+#sudo apt-get install build-essential g++ python-dev autotools-dev libicu-dev libbz2-dev 
 mkdir soft
 cd soft
 wget -O boost_1_60_0.tar.gz https://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.tar.gz/download
